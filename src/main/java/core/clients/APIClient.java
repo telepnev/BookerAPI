4531b7a93bf065cd8027ec77708b3ad6a132bbe4
Evgeny Telepnev <telepnef@yandex.ru>
2024-10-30 08:10:11 +0300

    added APIClient

---
+++ src/main/java/core/clients/APIClient.java
@@ -0,0 +1,32 @@
+package core.clients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class APIClient {
+    final private String baseUrl;
+
+    public APIClient() {
+        this.baseUrl = determineBaseUrl();
+    }
+
+    private String determineBaseUrl() {
+
+        String environment = System.getProperty("env", "test");
+        String configFileName = "application-" + environment + ".properties";
+
+        Properties properties = new Properties();
+        try (InputStream input =
+                     getClass().getClassLoader().getResourceAsStream(configFileName)) {
+            if (input == null) {
+                throw new IllegalStateException("Configuration file not found: "
+                        + configFileName);
+            }
+            properties.load(input);
+        } catch (IOException e) {
+            throw new IllegalStateException("Unable to load configuration file: " + configFileName, e);
+        }
+        return properties.getProperty("baseUrl");
+    }
+}
